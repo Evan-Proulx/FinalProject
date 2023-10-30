e439984ad13766a00e14094868eecab3f34ac15a
Evan <evanproulx7@gmail.com>
2023-10-30 16:51:52 -0400

    style.css file added

--- src/main/java/com/example/butcherbuddy/LoginPane.java
+++ src/main/java/com/example/butcherbuddy/LoginPane.java
@@ -1,7 +1,12 @@
 package com.example.butcherbuddy;
 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class LoginPane extends BorderPane {
+    public LoginPane(){
 
+    }
 }
